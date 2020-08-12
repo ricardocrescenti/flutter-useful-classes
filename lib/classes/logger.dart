d4293b103b6ae2bb79a64ac510888bc082625a9f
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2020-08-12 08:07:06 -0300

    Updated code formatting

--- lib/classes/logger.dart
+++ lib/classes/logger.dart
@@ -1,36 +1,35 @@
 import 'package:flutter/foundation.dart';
 
 /// The `Logger` class is a simple class for printing information on the terminal only when the application is in debug mode.
-/// 
+///
 /// Below is an example of implementing the logger.
-/// 
+///
 /// ```dart
 /// final Logger logger = Logger();
-/// 
+///
 /// logger.log('simple log');
 /// /// output: simple log
-/// 
+///
 /// logger.info('information log');
 /// /// output: 💡 information log
-/// 
+///
 /// logger.warning('warning log');
 /// /// output: ⚠️ warning log
-/// 
+///
 /// logger.error('error log');
 /// /// output: ⛔ error log
 /// ```
-/// 
-/// If it is necessary to add a prefix when printing all logs, use the [prefix] 
+///
+/// If it is necessary to add a prefix when printing all logs, use the [prefix]
 /// parameter in the class's constructor.
 class Logger {
-
   /// Logger [prefix].
-  /// 
+  ///
   /// This prefix will be printed at the beginning of any printed log.
   final String prefix;
 
   /// Standard constructor of the Logger class
-  Logger({ this.prefix });
+  Logger({this.prefix});
 
   /// Indicates if the app is running in debug mode
   final bool inDebugMode = !kReleaseMode;
@@ -58,4 +57,3 @@ class Logger {
     }
   }
 }
-
--- lib/classes/on_dispose.dart
+++ lib/classes/on_dispose.dart
@@ -2,32 +2,31 @@
 
 import 'package:flutter/foundation.dart';
 
-/// This class allows you to implement listeners notification when disposing 
+/// This class allows you to implement listeners notification when disposing
 /// object.
-/// 
+///
 /// To notify listeners, create a `dispose()` method in your class.
-/// 
+///
 /// ```dart
 /// class Test with OnDispose {
 ///     // your class structure
-/// 
+///
 ///     @override
 ///     dispose() {
 ///         // dispose your class and call `super.dispose()` to notify listeners
 ///         super.dispose();
 ///     }
 /// }
-/// 
+///
 /// final Test test = Test();
 /// test.onDispose.add(() => print('Object disposed'));
-/// 
+///
 /// test.dispose();
 /// ```
 abstract class OnDispose {
-
   /// List of methods that will be called when the object is dropped.
   final List<Function(dynamic)> onDispose = List();
-  
+
   /// Method that discards the object and notifies listeners
   @mustCallSuper
   dispose() {
